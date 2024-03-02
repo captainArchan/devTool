89bd926f3ec2d460c1fd4dd450944bb983f56f24
PhannitaSaisorn <pang.pntt@gmail.com>
2024-03-02 22:23:17 +0700

    sidebar responsive

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,84 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <SidebarMobile v-if="mobileView && showNav" />
+    <div class="content" :class="{'open': showNav}">
+      <div class="top-bar">
+        <div class="navigation-icon" v-if="mobileView" @click="showNav = !showNav">
+          <i class="toggle"><font-awesome-icon icon="bars" /></i>
+        </div>
+        <Sidebar v-if="!mobileView"/>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import Sidebar from './components/Sidebar.vue';
+import SidebarMobile from './components/SidebarMobile.vue';
+export default {
+  data: () => {
+    return {
+      mobileView: true,
+      showNav: false
+    };
+  },
+  components: {
+    Sidebar, SidebarMobile
+  },
+  methods: {
+    handleView(){
+      this.mobileView = window.innerWidth <= 990;
+    }
+  },
+  created() {
+    this.handleView();
+    window.addEventListener('resize', this.handleView);
+  }
+}
+</script>
+
+<style>
+
+body {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background-color: #D2E7F9;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app {
+  position: relative;
+  width: calc(100% - 20px);
+  height: calc(100vh - 20px);
+  padding: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.topbar {
+  display: flex;
+  width: 100%;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.navigation-icon {
+  padding: 10px 10px 20px;
+  margin-right: 10px;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.content {
+  position: absolute;
+  top: 10px;
+  width: calc(100% - 60px);
+  height: calc(100vh - 60px);
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 30px;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  transition: 1s transform cubic-bezier(0,.12,.14,1);
+}
+
+.open {
+  transform: translateX(300px);
 }
 </style>

4c3a843d33b857e3d13c98955e455ba6b5153cd9
PhannitaSaisorn <pang.pntt@gmail.com>
2024-03-04 19:08:30 +0700

    register

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/LogIn.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
     }
   ]
 })

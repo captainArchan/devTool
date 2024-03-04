3f35de5a8c1710ceb081a961cd37e1e500824af2
PhannitaSaisorn <pang.pntt@gmail.com>
2024-03-04 14:57:29 +0700

    update update last la

--- frontend/src/components/SidebarMobile.vue
+++ frontend/src/components/SidebarMobile.vue
@@ -1,34 +1,34 @@
 <template>
-  <div id="navigation-mobile">
-    <ul>
-      <li @click="showNav = false">FoonPM</li>
-      <li class="icon"><font-awesome-icon icon="map" /> ค้นหาสถานที่</li>
-      <li class="button">เข้าสู่ระบบ</li>
-    </ul>
-  </div>
-</template>
+    <div id="navigation-mobile">
+      <ul>
+        <li>FoonPM</li>
+        <li class="icon"><font-awesome-icon icon="map" /> ค้นหาสถานที่</li>
+        <li class="button">เข้าสู่ระบบ</li>
+      </ul>
+    </div>
+  </template>
+    
+  <script>
+  export default {
+  };
+  </script>
+    
+    
+  <style scoped>
+  #navigation-mobile {
+    padding: 20px 0 0 0;
+  }
   
-<script>
-export default {
-};
-</script>
+  ul {
+    list-style: none;
+    width: 200px;
+    padding-left: 40px;
+  }
   
-  
-<style scoped>
-#navigation-mobile {
-  padding: 20px 0 0 0;
-}
-
-ul {
-  list-style: none;
-  width: 200px;
-  padding-left: 40px;
-}
-
-li {
-  font-weight: bold;
-  margin-bottom: 20px;
-  cursor: pointer;
-}
-</style>
-  
+  li {
+    font-weight: bold;
+    margin-bottom: 20px;
+    cursor: pointer;
+  }
+  </style>
+    
